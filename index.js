87067512bae55c626c4fcfd08f5a8cb87e1dfaae
Marco <marcoenriquez56129@gmail.com>
2023-07-14 10:46:55 -0500

    Tarea s14

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function suma(a, b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let x = 5;
+let y = 3;
+let resultadoSuma = suma(x, y);
+console.log(resultadoSuma); // Imprimirá 8
