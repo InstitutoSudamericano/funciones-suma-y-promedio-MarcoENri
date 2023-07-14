5303f3547caf925d7125950fc781d2097c264462
Marco <marcoenriquez56129@gmail.com>
2023-07-14 10:51:15 -0500

    Tarea s14 3

---
+++ index2.js
@@ -0,0 +1,14 @@
+function verificarAprobacion(nota1,nota2,nota3){
+    const promedio=(nota1 + nota2 +nota3)/3; 
+    if(promedio> 70){
+    return "aprobado";
+    } else{
+        return "reprobado";
+    }
+    }
+    let nota1= 80;
+    let nota2= 75;
+    let nota3= 85;
+    
+    const resultado = verificarAprobacion(nota1, nota2, nota30);
+    console.log(resultado);
